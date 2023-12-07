dcfd71bd6c1dd0e13b05b2845d76eb350e29d589
giorgi <giorgisonidze8@gmail.com>
2023-12-07 11:57:01 +0400

    first commit

---
+++ homepage/home.js
@@ -0,0 +1,31 @@
+
+function openNav() {
+    document.getElementById("opennav_content").style.marginLeft = "-12px";
+}
+function closeNav() {
+    document.getElementById("opennav_content").style.marginleft = "-300px";
+
+}
+
+function entersignup() {
+    document.getElementById("body_h2").style.display = "none";
+
+    document.getElementById("opennav_content").style.display = "none";
+
+    document.getElementById("mainDiv").style.display = "none";
+
+    document.getElementById("registration_page").style.display = "block";
+    document.getElementById("mainpageimg").style.display = "none";
+
+}
+
+function enteruser() {
+    document.getElementById("registration_page").style.marginTop = "1000px"
+    document.getElementById("registration_page").style.transition = "2.5s"
+    
+    
+    
+}
+
+
+
